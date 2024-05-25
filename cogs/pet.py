eb8c8a70861003b6ff04b51891ad8a6ff03d3a7d
Phutawan Chanrueng <phutawanchanrueng@gmail.com>
2024-05-25 22:19:44 +0700

    Add petpet generator
    Test on Production

--- cogs/pet.py
+++ cogs/pet.py
@@ -3,22 +3,23 @@ from discord.ext import commands
 from discord import app_commands
 import utils.img_processsing as img_processsing
 import utils.filesize as filesize
+from petpetgif import petpet
 
-class Deepfry(commands.Cog):
+class Pet(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
         self.log_cog = client.get_cog("Log")
         self.context_menu = app_commands.ContextMenu(
-            name='Deepfry',
-            callback=self.deepfry,
+            name='Pet',
+            callback=self.pet,
         )
         self.client.tree.add_command(self.context_menu)
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print("Deepfry cog loaded")
+        print("Pet cog loaded")
 
-    async def deepfry(self, interaction: discord.Interaction, message: discord.Message):
+    async def pet(self, interaction: discord.Interaction, message: discord.Message):
         try:
             await self.log_cog.sendlog(interaction, data={'content': message.attachments[0].filename})
         except IndexError:
@@ -26,12 +27,12 @@ class Deepfry(commands.Cog):
             return
 
         await interaction.response.send_message("<a:AppleLoadingGIF:1052465926487953428> **กำลังสร้าง...**")
-        img_processsing.save_image_from_url(message.attachments[0].url, f"temp/deepfry/deepfryer_input/{message.attachments[0].filename}")
-        img_processsing.deepfry(f"temp/deepfry/deepfryer_input/{message.attachments[0].filename}")
-
+        img_processsing.save_image_from_url(message.attachments[0].url, f"temp/image/{message.attachments[0].filename}")
+        petpet.make(f'temp/image/{message.attachments[0].filename}', f'temp/image/{get_file_name_only}_petpet.gif')
+        
         get_file_name_only = img_processsing.get_filename(message.attachments[0].url)[1]
-
-        path = f'temp/deepfry/deepfryer_output/{get_file_name_only}_deepfried.png'
+        
+        path = f'temp/image/{get_file_name_only}_petpet.gif'
         file_name = discord.File(path)
         await interaction.edit_original_response(content=f"✅ **สร้างเสร็จแล้ว `({filesize.getsize(path)})`**")
         
@@ -42,7 +43,7 @@ class Deepfry(commands.Cog):
             await interaction.edit_original_response(content=f"❌ **ไฟล์ใหญ่เกินไป `({filesize.getsize(path)})`**")
             await self.log_cog.runcomplete('⚠️')
             return
-
+        
 async def setup(client: commands.Bot):
-    print("Setting up Deepfry cog")
-    await client.add_cog(Deepfry(client))
+    print("Setting up Pet cog")
+    await client.add_cog(Pet(client))
